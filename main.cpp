2de72e95040b3777586e6702d319dd14788b4651
nam <phung9nam@gmail.com>
2023-05-13 22:22:22 +0700

    init

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <QCoreApplication>
+#include <QHttpPart>
+#include <iostream>
+using namespace std;
+
+
+
+int main(int argc, char *argv[])
+{
+
+
+    QCoreApplication a(argc, argv);
+
+    return a.exec();
+}
